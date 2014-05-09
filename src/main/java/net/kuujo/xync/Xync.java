d0b547211540ed7412280d122ae5eece374b002e
Jordan Halterman <jordan.halterman@gmail.com>
2014-05-08 17:02:50 -0700

    Deploy deployments to the default group by default rather than to any node.

--- src/main/java/net/kuujo/xync/Xync.java
+++ src/main/java/net/kuujo/xync/Xync.java
@@ -583,10 +583,7 @@ public class Xync extends Verticle {
    * Deploys a deployment.
    */
   private void doClusterDeploy(final Message<JsonObject> message) {
-    String group = message.body().getString("group");
-    if (group == null) {
-      doInternalDeploy(message);
-    } else {
+    String group = message.body().getString("group", DEFAULT_GROUP);
       vertx.eventBus().sendWithTimeout(String.format("%s.%s", cluster, group), message.body(), 30000, new Handler<AsyncResult<Message<JsonObject>>>() {
         @Override
         public void handle(AsyncResult<Message<JsonObject>> result) {
